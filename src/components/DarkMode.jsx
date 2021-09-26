a3b6e3b05c447cf9622bdff4d75769c33b64946f
Francisco Pessano <franpessano1@gmail.com>
2021-09-25 22:49:57 -0300

    Added dark mode color pallete

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const DarkMode = () => {
     return (
         <div className="dark-mode">
-            <input type="checkbox" id="dark-mode_toogle-switch"/>
+            <input type="checkbox" id="dark-mode_toogle-switch" onChange={() => {
+                
+            }}/>
             <label for="dark-mode_toogle-switch"></label>
         </div>
     )
