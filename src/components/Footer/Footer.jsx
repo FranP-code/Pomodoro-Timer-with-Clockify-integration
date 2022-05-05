97f1b1d1fd482b42ad2fb544248bf6ab094a8564
Francisco Pessano <franpessano1@gmail.com>
2022-05-05 20:45:06 -0300

    Featured on Product Hunt Button added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,8 @@ import React from 'react'
 const Footer = (props) => {
     return (
         <footer className={props.darkMode ? 'made-with-love dark-mode-component' : 'made-with-love'}>
-            Made with 💓 by <a href="https://porfolio-franp.netlify.app" target="_blank" rel="noreferrer">Francisco Pessano</a>
+            <p>Made with 💓 by <a href="https://porfolio-franp.netlify.app" target="_blank" rel="noreferrer">Francisco Pessano</a></p>
+            <a href="https://www.producthunt.com/posts/clockify-pomodoro-timer?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-clockify&#0045;pomodoro&#0045;timer" target="_blank" rel="noreferrer"><img src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=334653&theme=${localStorage.getItem('dark-mode') === "true" ? "dark" : "light"}`} alt="Clockify&#0032;Pomodoro&#0032;Timer - Pomodoro&#0032;Timer&#0032;that&#0032;sync&#0032;the&#0032;worked&#0032;hours&#0032;with&#0032;Clockify | Product Hunt"/></a>
         </footer>
     )
 }
